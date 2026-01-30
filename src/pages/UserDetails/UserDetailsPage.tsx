b48dee3d15cac71e66efe7796bf90ffd4bd160c2
okestra <generalchukzzy.co@gmail.com>
2026-01-30 01:05:40 +0100

    user type definition + json schema initialization

---
+++ src/pages/UserDetails/UserDetailsPage.tsx
@@ -0,0 +1,11 @@
+/*  User details page component
+    shows detailed information of selected user
+ */
+
+export default function UserDetailsPage() {
+    return (
+        <div>
+            <h1> User Details page</h1>
+        </div>
+    );
+}
